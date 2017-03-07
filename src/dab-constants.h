7eb8ab19c415bcb799e1cf5e9c279cc28afb00ff
Albrecht Lohofener <albrechtloh@gmx.de>
2017-03-07 22:50:36 +0100

    Removed unused code

--- src/dab-constants.h
+++ src/dab-constants.h
@@ -45,25 +45,6 @@
 
 #include <QString>
 
-//#if defined QWT_VERSION && ((QWT_VERSION >> 8) > 0x0601)
-//#define	QT_STATIC_CONST
-//#endif
-
-#ifndef __FREEBSD__
-#include <malloc.h>
-#endif
-
-#ifdef __MINGW32__
-//#include	"iostream.h"
-#include "windows.h"
-#else
-#ifndef __FREEBSD__
-#include "alloca.h"
-#endif
-#include "dlfcn.h"
-typedef void* HINSTANCE;
-#endif
-
 typedef float DSPFLOAT;
 typedef std::complex<DSPFLOAT> DSPCOMPLEX;
 
@@ -94,47 +75,15 @@ using namespace std;
 #define LONG_LOW 03
 #define UNSYNCED 04
 
-static inline bool isIndeterminate(DSPFLOAT x)
-{
-    return x != x;
-}
-
-static inline bool isInfinite(double x)
-{
-    return x == numeric_limits<DSPFLOAT>::infinity();
-}
-
 static inline DSPCOMPLEX cmul(DSPCOMPLEX x, float y)
 {
     return DSPCOMPLEX(real(x) * y, imag(x) * y);
-}
-
-static inline DSPCOMPLEX cdiv(DSPCOMPLEX x, float y)
-{
-    return DSPCOMPLEX(real(x) / y, imag(x) / y);
 }
 
 static inline float get_db(DSPFLOAT x)
 {
     return 20 * log10((x + 1) / (float)(256));
 }
-//
-static inline DSPFLOAT PI_Constrain(DSPFLOAT val)
-{
-    if (0 <= val && val < 2 * M_PI)
-        return val;
-    if (val >= 2 * M_PI)
-        return fmod(val, 2 * M_PI);
-    //	apparently val < 0
-    if (val > -2 * M_PI)
-        return val + 2 * M_PI;
-    return 2 * M_PI - fmod(-val, 2 * M_PI);
-}
-/*
- */
-
-#define MINIMUM(x, y) ((x) < (y) ? x : y)
-#define MAXIMUM(x, y) ((x) > (y) ? x : y)
 
 static inline float jan_abs(DSPCOMPLEX z)
 {
