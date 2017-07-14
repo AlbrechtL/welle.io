0d23d93a5c24938592225631419858bc921f6652
Matthias P. Braendli <matthias.braendli@mpb.li>
2017-07-14 16:33:43 +0200

    Use vectors in Xtan2

--- src/various/Xtan2.h
+++ src/various/Xtan2.h
@@ -1,35 +1,35 @@
-#
 //
-//	This LUT implementation of atan2 is a C++ translation of
-//	a Java discussion on the net
-//	http://www.java-gaming.org/index.php?topic=14647.0
+//  This LUT implementation of atan2 is a C++ translation of
+//  a Java discussion on the net
+//  http://www.java-gaming.org/index.php?topic=14647.0
 
-#ifndef		__COMP_ATAN
-#define		__COMP_ATAN
+#ifndef     __COMP_ATAN
+#define     __COMP_ATAN
 
-#include	<math.h>
-#include	<stdio.h>
-#include	<stdint.h>
-#include	<cstdlib>
-#include	<limits>
-#include	"DabConstants.h"
-#
-class	compAtan {
-public:
-		compAtan	(void);
-		~compAtan	(void);
-	float	atan2		(float, float);
-	float	argX		(DSPCOMPLEX);
-private:
-	float	*ATAN2_TABLE_PPY;
-	float	*ATAN2_TABLE_PPX;
-	float	*ATAN2_TABLE_PNY;
-	float	*ATAN2_TABLE_PNX;
-	float	*ATAN2_TABLE_NPY;
-	float	*ATAN2_TABLE_NPX;
-	float	*ATAN2_TABLE_NNY;
-	float	*ATAN2_TABLE_NNX;
-	float	Stretch;
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <cstdlib>
+#include <limits>
+#include <vector>
+#include "DabConstants.h"
+
+class compAtan
+{
+    public:
+        compAtan(void);
+        float   atan2(float y, float x);
+        float   argX(DSPCOMPLEX);
+    private:
+        std::vector<float> ATAN2_TABLE_PPY;
+        std::vector<float> ATAN2_TABLE_PPX;
+        std::vector<float> ATAN2_TABLE_PNY;
+        std::vector<float> ATAN2_TABLE_PNX;
+        std::vector<float> ATAN2_TABLE_NPY;
+        std::vector<float> ATAN2_TABLE_NPX;
+        std::vector<float> ATAN2_TABLE_NNY;
+        std::vector<float> ATAN2_TABLE_NNX;
+        float   Stretch;
 };
 
 #endif
