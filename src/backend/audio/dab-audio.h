59ad86a2a5c13d5b5727f518e89c1dad8bb2acc6
Matthias P. Braendli <matthias.braendli@mpb.li>
2017-07-14 14:44:59 +0200

    Revert "Use unique_ptr for dabProcessor"
    Because I don't understand the interaction between a QObject and
    a unique_ptr enough. Both seem to take ownership of the dabProcessor,
    and when the unique_ptr wants to delete it, we get a double free.
    
    This reverts commit dc994bbfa4f17c5701256f3ce4461ab48774c19f.

--- src/backend/audio/dab-audio.h
+++ src/backend/audio/dab-audio.h
@@ -68,7 +68,7 @@ class dabAudio : public QThread, public dabVirtual
         QMutex          ourMutex;
 
         std::unique_ptr<protection> protectionHandler;
-        std::unique_ptr<dabProcessor> our_dabProcessor;
+        dabProcessor   *our_dabProcessor;
         RingBuffer<int16_t> *Buffer;
 };
 
