5708bfad6ea1e2fadc5d1d9db39f99422302cc0f
Albrecht Lohofener <albrechtloh@gmx.de>
2018-02-13 22:31:00 +0100

    Add read of tuner type from rtl_tcp

--- src/input/CRTL_TCP_Client.h
+++ src/input/CRTL_TCP_Client.h
@@ -43,6 +43,12 @@
 #include "ringbuffer.h"
 #include "CRadioController.h"
 
+typedef struct { /* structure size must be multiple of 2 bytes */
+    char magic[4];
+    uint32_t tuner_type;
+    uint32_t tuner_gain_count;
+} dongle_info_t;
+
 class CRTL_TCP_Client : public CVirtualInput {
     Q_OBJECT
 public:
@@ -96,6 +102,26 @@ private:
     bool stopped;
     QHostAddress serverAddress;
     uint16_t serverPort;
+
+    bool FirstData;
+    dongle_info_t DongleInfo;
+
+    // Gain values for the different tuners
+    const std::array<float, 14> e4k_gains {{-1.0, 1.5, 4.0, 6.5, 9.0, 11.5, 14.0, 16.5, 19.0, 21.5,
+            24.0, 29.0, 34.0, 42.0}};
+
+    const std::array<float, 5> fc0012_gains {{ -9.9, -4.0, 7.1, 17.9, 19.2 }};
+
+    const std::array<float, 23>  fc0013_gains {{ -9.9, -7.3, -6.5, -6.3, -6.0, -5.8, -5.4, 5.8, 6.1,
+                       6.3, 6.5, 6.7, 6.8, 7.0, 7.1, 17.9, 18.1, 18.2,
+                       18.4, 18.6, 18.8, 19.1, 19.7 }};
+
+    const std::array<float, 1>  fc2580_gains {{ 0 /* no gain values */ }};
+
+    const std::array<float, 29>  r82xx_gains {{ 0.0, 9.0, 1.4, 2.7, 3.7, 7.7, 8.7, 12.5, 14.4, 15.7,
+                     16.6, 19.7, 20.7, 22.9, 25.4, 28.0, 29.7, 32.8,
+                     33.8, 36.4, 37.2, 38.6, 40.2, 42.1, 43.4, 43.9,
+                     44.5, 48.0, 49.6 }};
 
     void sendVFO(int32_t frequency);
     void sendRate(int32_t theRate);
