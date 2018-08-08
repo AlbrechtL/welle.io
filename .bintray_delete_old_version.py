5fe165d8b703b9b7717d6e51cacdb85f5b6f763c
Albrecht Lohofener <albrechtloh@gmx.de>
2018-08-08 15:19:46 +0200

    Add script to delete old binaries on bintray

---
+++ .bintray_delete_old_version.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import sys
+
+bintray_api = 'https://api.bintray.com'
+#bintray_user = 'albrechtl'
+#bintray_pass = '<secret>'
+bintray_package = '/packages/albrechtl/welle.io/welle.io_next_nightly'
+keep_last_version = 20
+
+if len(sys.argv) != 2 + 1:
+    print('Error: The number of parameters is != 4')
+    exit(-1)
+
+bintray_user = sys.argv[1]
+bintray_pass = sys.argv[2]
+
+# Get all versions
+r = requests.get(bintray_api + bintray_package, auth=(bintray_user, bintray_pass))
+
+# Decode data
+data = json.loads(r.text)
+
+# Extract versions
+versions = data['versions']
+
+# Extract versions to delete
+delete_versions = versions[keep_last_version:len(versions)]
+
+# Iterate over all versions to delete
+for version in delete_versions:
+    print('Deleting version ' + version)
+
+    # Delete version
+    r = requests.delete(bintray_api + bintray_package + '/versions/' + version, auth=(bintray_user, bintray_pass))
+
+    # Check status
+    if r.status_code != 200:
+        print(f'Error while deleting. Status {r.status_code}')
+    else:
+        delete_status = json.loads(r.text)
+        print('Status: ' + delete_status['message'])
+	
