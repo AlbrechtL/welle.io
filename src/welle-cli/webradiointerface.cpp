bf6457485394ad7da16087b02029b3db3bdc067a
Matthias Kollmann <matthias.kollmann@iosb.fraunhofer.de>
2019-01-04 23:46:00 +0100

    fix for missing ```MSG_NOSIGNAL```

--- src/welle-cli/webradiointerface.cpp
+++ src/welle-cli/webradiointerface.cpp
@@ -36,6 +36,16 @@
 #include "welle-cli/webradiointerface.h"
 #include "libs/json.hpp"
 
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+
+#ifdef GITDESCRIBE
+#define VERSION GITDESCRIBE
+#else
+#define VERSION "unknown"
+#endif
+
 using namespace std;
 
 static const char* http_ok = "HTTP/1.0 200 OK\r\n";
@@ -413,7 +423,7 @@ bool WebRadioInterface::send_mux_json(Socket& s)
     nlohmann::json j;
 
     j["receiver"]["software"]["name"] = "welle.io";
-    j["receiver"]["software"]["version"] = GITDESCRIBE;
+    j["receiver"]["software"]["version"] = VERSION;
     j["receiver"]["hardware"]["name"] = input.getDescription();
     j["receiver"]["hardware"]["gain"] = input.getGain();
 
