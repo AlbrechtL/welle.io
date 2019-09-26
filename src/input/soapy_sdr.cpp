532fe91077968306a883525cabba301cce0ef170
Matthias P. Braendli <matthias.braendli@mpb.li>
2019-09-26 14:08:23 +0200

    Revert wrong change in soapy_sdr.cpp

--- src/input/soapy_sdr.cpp
+++ src/input/soapy_sdr.cpp
@@ -76,7 +76,7 @@ bool CSoapySdr::restart()
     m_sampleBuffer.FlushRingBuffer();
     m_spectrumSampleBuffer.FlushRingBuffer();
 
-    m_device = SoapySDR::Device::make("driver=uhd");
+    m_device = SoapySDR::Device::make(m_driver_args);
     stringstream ss;
     ss << "SoapySDR driver=" << m_device->getDriverKey();
     ss << " hardware=" << m_device->getHardwareKey();
