6b6675e6794d29098d71864b876b793d1b78afb8
Albrecht Lohofener <albrechtloh@gmx.de>
2020-02-13 17:56:06 +0100

    Fix welle-cli Windows build

--- src/welle-cli/webradiointerface.cpp
+++ src/welle-cli/webradiointerface.cpp
@@ -38,7 +38,13 @@
 #include <regex>
 #include <signal.h>
 #include <stdexcept>
+#if defined(_WIN32)
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <windows.h>
+#else
 #include <sys/socket.h>
+#endif
 #include <utility>
 #include "Socket.h"
 #include "channels.h"
